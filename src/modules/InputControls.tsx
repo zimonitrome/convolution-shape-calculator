843fef2251f05302c576b650a2603ab06c0f3f1d
Filipandersson245 <odin@pop-os.localdomain>
2022-05-21 15:43:14 +0200

    Missed some changes.

--- src/modules/InputControls.tsx
+++ src/modules/InputControls.tsx
@@ -6,11 +6,11 @@ export const [inputChannels, setinputChannels] = createSignal(3);
 
 export default (props: any) => {
     return <div>
-          <input type="range" min="1" max="64" value="0" onInput={(e) => { setInputWidth(parseInt(e.currentTarget.value)) }}/>
+          <input type="range" min="1" max="64" value={inputWidth()} onInput={(e) => { setInputWidth(parseInt(e.currentTarget.value)) }}/>
           <output>{inputWidth()}</output>
-          <input type="range" min="1" max="64" onChange={setInputHeight}/>
+          <input type="range" min="1" max="64" value={inputHeight()} onInput={(e) => { setInputHeight(parseInt(e.currentTarget.value)) }}/>
           <output>{inputHeight()}</output>
-          <input type="range" min="1" max="64" onChange={setinputChannels}/>          
+          <input type="range" min="1" max="64" value={inputChannels()} onInput={(e) => { setinputChannels(parseInt(e.currentTarget.value)) }}/>          
           <output>{inputChannels()}</output>
     </div>
 }
