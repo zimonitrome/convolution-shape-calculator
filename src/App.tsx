89d1cffe58061dddf747d62659f23720640e5210
Filipandersson245 <odin@pop-os.localdomain>
2022-05-21 16:33:04 +0200

    Moves around some css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from './index.css';
 
 import InputControls from './modules/InputControls';
 import FilterControls from './modules/FilterControls';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+    <div class="App">
+      <header class="header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
