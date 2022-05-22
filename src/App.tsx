5fd76bbde8a9535f20b28d679933d24cc7e8dec1
zimonitrome <zimonitrome@gmail.com>
2022-05-22 14:10:02 +0200

    Added footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import type { Component } from 'solid-js';
 import Conv2d from './modules/Conv2d';
 import DownArrow from './modules/DownArrow';
+import Footer from './modules/Footer';
 import InputShape from './modules/InputShape';
 import OutputShape from './modules/OutputShape';
 
 const App: Component = () => {
-  return (
+  return <>
     <div class="App">
-      <InputShape/>
-      <DownArrow height={50} width={60}/>
-      <Conv2d/>
-      <DownArrow height={50} width={60}/>
-      <OutputShape/>
+      <InputShape />
+      <DownArrow height={50} width={60} />
+      <Conv2d />
+      <DownArrow height={50} width={60} />
+      <OutputShape />
     </div>
-  );
+    <Footer/>
+  </>;
 };
 
 export default App;
---
+++ src/modules/Footer.tsx
@@ -0,0 +1,7 @@
+export default (props: any) => <div style={{
+    "padding-left": "15px",
+    "margin": "0 auto",
+    "width": "fit-content"
+}}>
+    <p>Github repo: <a href='https://github.com/zimonitrome/convolution-shape-calculator'>https://github.com/zimonitrome/convolution-shape-calculator</a></p>
+</div>
