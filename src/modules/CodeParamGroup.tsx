4aa24f86ad6182bcc2e6fa067d5649e06f52246b
zimonitrome <zimonitrome@gmail.com>
2022-05-22 15:32:56 +0200

    Better wraps CodeParams for narrow screens.

--- src/modules/CodeParamGroup.tsx
+++ src/modules/CodeParamGroup.tsx
@@ -1,18 +1,25 @@
 import { JSX } from "solid-js";
 
 export default (props: any) => {
-    const codeStyle: JSX.CSSProperties = {
+    const outerContainerStyle: JSX.CSSProperties = {
         "font-weight": "bold",
         "font-family": "ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace",
         "display": "flex",
         "flex-direction": "row",
-        "flex-wrap": "wrap",
         "justify-content": "center",
         "white-space": "nowrap",
         "font-size": "18pt",
     }
 
-    return <div style={codeStyle}>
-        {props.children}
+    const innerContainerStyle: JSX.CSSProperties = {
+        "display": "flex",
+        "flex-wrap": "wrap",
+    }
+
+    return <div style={outerContainerStyle}>
+        {props.children[0]}
+        <div style={innerContainerStyle}>
+            {props.children.slice(1)}
+        </div>
     </div>
 }
