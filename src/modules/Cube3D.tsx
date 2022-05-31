d44ae66180433960911382455aa6551f3b2c3118
zimonitrome <zimonitrome@gmail.com>
2022-05-31 13:34:28 +0200

    inprog 4

--- src/modules/Cube3D.tsx
+++ src/modules/Cube3D.tsx
@@ -4,11 +4,11 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ObjectControls } from 'threeJS-object-controls';
 import { LineMaterial } from "three/examples/jsm/lines/LineMaterial";
 import { LineSegmentsGeometry } from "three/examples/jsm/lines/LineSegmentsGeometry";
-import { MeshPhongMaterial, MeshStandardMaterial, PlaneGeometry } from 'three';
+import { Float32BufferAttribute, MeshPhongMaterial, MeshStandardMaterial, PlaneGeometry, Uint16BufferAttribute } from 'three';
 import { outputWidth, outputHeight } from './OutputShape';
 import { dilation, kernelSize, outputChannels, padding, stride } from './Conv2d';
 
-interface SvgCubeProps {
+interface Cube3DProps {
     channels: number;
     height: number;
     width: number;
@@ -61,8 +61,6 @@ function getTextSprite(text: string, position: [number, number, number], fontsiz
 
 function canvas2DToWorld3D(canvasX: number, canvasY: number, camera: THREE.Camera, canvas: HTMLCanvasElement, scene: THREE.Scene) {
     let vector = new THREE.Vector3();
-    console.log(canvas.width);
-    console.log(canvas.height);
     vector.set(
         (canvasX / canvas.width) * 2 - 1,
         - (canvasY / canvas.height) * 2 + 1,
@@ -71,7 +69,7 @@ function canvas2DToWorld3D(canvasX: number, canvasY: number, camera: THREE.Camer
     return vector.unproject(camera);
 }
 
-function getCube(w: number, h: number, c: number, colors: Array<string>, borderColor: string) {
+function getCube(w: number, h: number, c: number, colors: Array<string>, borderColor: string, aspect: number = 1) {
     const group = new THREE.Group();
     // group.position.x = 0;
     // group.position.y = 0;
@@ -162,6 +160,7 @@ function getCube(w: number, h: number, c: number, colors: Array<string>, borderC
 
     // // // OPENGL OUTLINES
     // let edges = new THREE.EdgesGeometry(geometry);
+    // // edges.scale(1.05, 1.05, 1.05); // Scale slightly to svoid clipping
     // let lines = new LineSegmentsGeometry().fromEdgesGeometry(edges);
     // let borderColors = [];
     // for (let i = 0; i < lines.attributes.instanceStart.count; i++) {
@@ -171,12 +170,15 @@ function getCube(w: number, h: number, c: number, colors: Array<string>, borderC
     // var mat = new LineMaterial({
     //     color: new THREE.Color(borderColor).getHex(),
     //     // color: 0xffffff,
-    //     linewidth: 0.005,
+    //     linewidth: 0.003,
     //     vertexColors: true,
-    //     worldUnits: false
+    //     worldUnits: false,
+    //     resolution: new THREE.Vector2(aspect, 1),
+    //     // clipping: true
     // });
     // let outlineMesh = new THREE.Mesh(lines, mat)
     // group.add(outlineMesh);
+
 
     const outlineWidth = 0.02;
     const brCube = new THREE.Mesh(
@@ -199,31 +201,115 @@ function getCube(w: number, h: number, c: number, colors: Array<string>, borderC
     return group
 }
 
-function getConv2d(inputChannels: number, outputChannels: number, kernelSize:number, stride: number, padding:number, dilation:number) {
+function getConv2d(inputChannels: number, outputChannels: number, kernelSize: number, stride: number, padding: number, dilation: number) {
     const group = new THREE.Group();
 
-    var geometry = new THREE.CylinderGeometry( 1 / Math.sqrt( 2 ), 0.7 / Math.sqrt( 2 ), 1, 4, 1 ); // size of top can be changed
-    geometry.rotateY( Math.PI / 4 );
+    var geometry = new THREE.CylinderGeometry(1 / Math.sqrt(2), 0.7 / Math.sqrt(2), 1, 4, 1); // size of top can be changed
+    geometry.rotateY(Math.PI / 4);
     geometry = geometry.toNonIndexed(); // removes shared vertices
     geometry.computeVertexNormals(); // normals will be 'flat' normals
-    let mesh = new THREE.Mesh( geometry, new MeshStandardMaterial({color:0xff0000, opacity: 0.6, transparent: true, side: THREE.BackSide}) );
+    let mesh = new THREE.Mesh(geometry, new MeshStandardMaterial({ color: 0xff0000, opacity: 0.6, transparent: true, side: THREE.BackSide }));
     // mesh.scale.set( 1.5, 1, 1 );
     mesh.scale.multiply(new THREE.Vector3(1, 1, 1.5));
     mesh.renderOrder = -1;
     group.add(mesh);
 
-    
-    let cube = getCube(kernelSize,kernelSize,outputChannels, ["#E22030", "#00AEAC", "#136371", "#F2B995"], "#1E3A4B");
+
+    let cube = getCube(kernelSize, kernelSize, outputChannels, ["#E22030", "#00AEAC", "#136371", "#F2B995"], "#1E3A4B");
     cube.scale.multiplyScalar(0.7);
     cube.renderOrder = 1;
     group.add(cube);
 
 
     return group
-    
+
 }
 
-export const Cube3D = (inProps: SvgCubeProps) => {
+function getThinBox({ w = 1, h = 1, d = 1 }) {
+    let boxgeom = new THREE.BoxGeometry(w, h, d);
+    let edgesgeom = new THREE.EdgesGeometry(boxgeom);
+    var mat = new THREE.LineBasicMaterial({
+        color: 0x000000,
+    });
+    return new THREE.LineSegments(edgesgeom, mat)
+}
+
+function getBox({ w = 1, h = 1, d = 1, linewidth = 0.002, aspect = 1 }) {
+    let boxgeom = new THREE.BoxGeometry(w, h, d);
+    let edgesgeom = new THREE.EdgesGeometry(boxgeom);
+
+    let lines = new LineSegmentsGeometry().fromEdgesGeometry(edgesgeom);
+    let borderColors = [];
+    for (let i = 0; i < lines.attributes.instanceStart.count; i++) {
+        borderColors.push(1, 1, 1, 1, 1, 1)
+    }
+    lines.setColors(borderColors);
+    var mat = new LineMaterial({
+        color: new THREE.Color(0x000000).getHex(),
+        linewidth: linewidth,
+        vertexColors: true,
+        worldUnits: false,
+        resolution: new THREE.Vector2(1 * aspect, 1)
+    });
+    return new THREE.Mesh(lines, mat)
+}
+
+function getCubeBox({ cubeW = 1, cubeH = 1, cubeC = 1, cubeScale = 0.3, cubeCenter = new THREE.Vector3(0, 0, 0), wSpan = [0, 1], hSpan = [0, 1], cSpan = [0, 1], aspect = 1, selfScale = 0.05 }) {
+    const group = new THREE.Group();
+
+    let h_spatial = cubeH == 1 ? 0.35 : Math.log(cubeH)
+    let w_spatial = cubeW == 1 ? 0.35 : Math.log(cubeW)
+    let c_spatial = cubeC == 1 ? 0.35 : Math.log(cubeC)
+
+    let max_side = Math.max(h_spatial, w_spatial, c_spatial, 1.6)
+
+    h_spatial = (h_spatial / max_side) * cubeScale
+    w_spatial = (w_spatial / max_side) * cubeScale
+    c_spatial = (c_spatial / max_side) * cubeScale
+
+    let voxelSizeH = h_spatial / cubeH;
+    let voxelSizeW = w_spatial / cubeW;
+    let voxelSizeC = c_spatial / cubeC;
+
+    let cubeLeftTopBack = cubeCenter.clone().sub(new THREE.Vector3(w_spatial / 2, -h_spatial / 2, c_spatial / 2));
+
+    let boxFromW = cubeLeftTopBack.x + (wSpan[0] * voxelSizeW);
+    let boxToW = cubeLeftTopBack.x + (wSpan[1] * voxelSizeW);
+    let boxFromH = cubeLeftTopBack.y + (hSpan[0] * voxelSizeH);
+    let boxToH = cubeLeftTopBack.y + (hSpan[1] * voxelSizeH);
+    let boxFromC = cubeLeftTopBack.z + (cSpan[0] * voxelSizeC);
+    let boxToC = cubeLeftTopBack.z + (cSpan[1] * voxelSizeC);
+
+    let box = getBox({ w: Math.abs(boxFromW - boxToW) + 0.01, h: Math.abs(boxFromH - boxToH) + 0.01, d: Math.abs(boxFromC - boxToC) + 0.01, aspect: aspect });
+
+    // box.position.set((boxFromW + boxToW) / 2, (boxFromH + boxToH) / 2, (boxFromC + boxToC) / 2);
+    box.position.set(cubeLeftTopBack.x - (boxFromW - boxToW) / 2, cubeLeftTopBack.y + (boxFromH - boxToH) / 2, cubeLeftTopBack.z - (boxFromC - boxToC) / 2);
+    // box.position.set(cubeCenter.x, cubeCenter.y, cubeCenter.z);
+    // box.scale.add(new THREE.Vector3(0.1, 0.1, 0.1))
+    // box.scale.x
+
+    group.add(box);
+
+    return box;
+}
+
+function getBboxVertecies(bbox: THREE.Box3) {
+    const low = bbox.min;
+    const high = bbox.max;
+
+    return [
+        new THREE.Vector3(low.x, low.y, low.z),
+        new THREE.Vector3(high.x, low.y, low.z),
+        new THREE.Vector3(low.x, high.y, low.z),
+        new THREE.Vector3(low.x, low.y, high.z),
+        new THREE.Vector3(high.x, high.y, low.z),
+        new THREE.Vector3(high.x, low.y, high.z),
+        new THREE.Vector3(low.x, high.y, high.z),
+        new THREE.Vector3(high.x, high.y, high.z),
+    ]
+}
+
+export const Cube3D = (inProps: Cube3DProps) => {
     const props = mergeProps({ colors: ['#FCBF49'], borderColor: "#003049" }, inProps)
 
     let elem: HTMLDivElement | undefined = undefined;
@@ -247,15 +333,15 @@ export const Cube3D = (inProps: SvgCubeProps) => {
             let width = elem.parentElement!.clientWidth;
             let height = elem.parentElement!.clientHeight;
             let aspect = width / height;
-            
+
             camera.left = -d * aspect;
             camera.right = d * aspect;
             camera.top = d;
             camera.bottom = -d;
             camera.updateProjectionMatrix();
             // camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 100000);
-        
-            renderer.setSize( width, height );
+
+            renderer.setSize(width, height);
         }
         window.addEventListener("resize", updateCanvas, false);
 
@@ -268,7 +354,7 @@ export const Cube3D = (inProps: SvgCubeProps) => {
         elem!.appendChild(canvas);
 
         camera.position.set(-20, 10, 20);
-        camera.lookAt(new THREE.Vector3(0,0,0));
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 
 
@@ -285,14 +371,12 @@ export const Cube3D = (inProps: SvgCubeProps) => {
         scene.add(inputCube);
 
 
-        let conv = getConv2d(1,1,1,1,1,1);
-        console.log("conv")
-        console.log(conv)
+        let conv = getConv2d(1, 1, 1, 1, 1, 1);
         conv.scale.multiplyScalar(.3)
         // conv.position.set(0,0,0)
         scene.add(conv);
 
-        
+
         let outputCube = getCube(1, 1, 1, props.colors, props.borderColor);
         scene.add(outputCube);
         // // cube2.position.set(0, -1, 0)
@@ -301,6 +385,12 @@ export const Cube3D = (inProps: SvgCubeProps) => {
         // const controls = new ObjectControls(camera, renderer.domElement, [cube, ...cube.children]);
 
         // const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
+
+
+
 
         var mouseDown = false,
             mouseX = 0,
@@ -316,15 +406,21 @@ export const Cube3D = (inProps: SvgCubeProps) => {
         function rotateAboutPoint(obj: THREE.Object3D, point: THREE.Vector3, axis: THREE.Vector3, theta: number, pointIsWorld: boolean = false) {
             pointIsWorld = (pointIsWorld === undefined) ? false : pointIsWorld;
 
+            if (pointIsWorld) {
+                obj.parent.localToWorld(obj.position); // compensate for world coordinate
+            }
             obj.position.sub(point); // remove the offset
             obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
             obj.position.add(point); // re-add the offset
+
+            if (pointIsWorld) {
+                obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+            }
 
             obj.rotateOnAxis(axis, theta); // rotate the OBJECT
         }
         document.addEventListener('mousemove', function (evt) {
             if (!mouseDown) { return }
-            //console.log('drag')
             evt.preventDefault();
             var deltaX = evt.clientX - mouseX,
                 deltaY = evt.clientY - mouseY;
@@ -336,6 +432,11 @@ export const Cube3D = (inProps: SvgCubeProps) => {
 
             conv.rotation.set(inputCube.rotation.x, inputCube.rotation.y, inputCube.rotation.z)
             outputCube.rotation.set(inputCube.rotation.x, inputCube.rotation.y, inputCube.rotation.z)
+            // mesh.rotation.set(inputCube.rotation.x, inputCube.rotation.y, inputCube.rotation.z)
+
+            selectBox.rotation.set(inputCube.rotation.x, inputCube.rotation.y, inputCube.rotation.z)
+            // rotateAboutPoint(selectBox, inputCube.position.clone(), new THREE.Vector3(0, 1, 0), toRadians(deltaX), true)
+            // rotateAboutPoint(selectBox, inputCube.position.clone(), new THREE.Vector3(1, 0, 0), toRadians(deltaY), true)
 
         }, false);
 
@@ -350,7 +451,19 @@ export const Cube3D = (inProps: SvgCubeProps) => {
             mouseDown = false;
         });
 
-        console.log(inputCube)
+        let selectBox = getCubeBox({});
+        scene.add(selectBox);
+
+
+
+        let mesh = getThinBox({});
+        mesh.scale.multiplyScalar(0.3);
+        mesh.rotateY(0.1);
+        scene.add(mesh);
+
+
+
+
 
 
 
@@ -363,9 +476,30 @@ export const Cube3D = (inProps: SvgCubeProps) => {
         dirLight.position.set(12, 18, 15);
         scene.add(dirLight);
 
+        // console.log(mesh)
+        // console.log(mesh)
+        // mesh.geometry.attributes.position = new Float32BufferAttribute([
+        //     -0.5, 0.5, -0.5, 
+        //     -0.5, 0.5, 0.5, 
+        //     0.5, 0.5, 0.5, 
+        //     0.5, 0.5, -0.5, 
+        //     -0.5, -0.5, 0.5, 
+        //     -0.5, -0.5, -0.5, 
+        // ], 3)
+        // console.log(mesh.geometry.attributes.position)
+
         function animate() {
+
+
             requestAnimationFrame(animate);
             renderer.render(scene, camera);
+
+            // console.log(outputCube.position)
+            mesh.geometry.attributes.position = new Float32BufferAttribute([
+                0,0,0,
+                // ...getBboxVertecies(conv.children[0]..geometry.boundingBox!)[0].toArray(),
+                ...mesh.worldToLocal(selectBox.localToWorld(getBboxVertecies(selectBox.geometry.boundingBox!)[0])).toArray()
+            ], 3)
         }
         animate();
 
@@ -375,10 +509,9 @@ export const Cube3D = (inProps: SvgCubeProps) => {
 
 
         var bbox = new THREE.Box3().setFromObject(inputCube);
-        
-        
+
+
         // scene.add(new THREE.PlaneHelper( new THREE.Plane().setFromNormalAndCoplanarPoint(camera.position, new THREE.Vector3(0,0,0)), 1, 0xffff00 ));
-        // console.log(pos)
         // var sphere = new THREE.Mesh(new THREE.SphereGeometry(0.1, 32, 16), new THREE.MeshPhongMaterial({ color: 0x0000ff }))
         // sphere.position.set(pos.x, pos.y, pos.z);
         // scene.add(sphere)
@@ -397,12 +530,34 @@ export const Cube3D = (inProps: SvgCubeProps) => {
                 let newInputCube = getCube(props.width, props.height, props.channels, props.colors, props.borderColor)
                 inputCube.remove(...inputCube.children);
                 inputCube.add(...newInputCube.children);
-                let pos = canvas2DToWorld3D(canvas.width / 2, 400/2, camera, canvas, scene);
+                let pos = canvas2DToWorld3D(canvas.width / 2, 400 / 2, camera, canvas, scene);
                 inputCube.position.set(pos.x, pos.y, pos.z);
-                inputCube.scale.set(0.3, 0.3, 0.3)
+                inputCube.scale.set(0.3, 0.3, 0.3);
             }
 
-            
+
+
+            scene.remove(selectBox);
+            selectBox = getCubeBox({
+                cubeW: props.width,
+                cubeH: props.height,
+                cubeC: props.channels,
+                cubeCenter: inputCube.position,
+                cubeScale: 0.3,
+                wSpan: [0, kernelSize()],
+                hSpan: [0, kernelSize()],
+                cSpan: [0, props.channels],
+                aspect: aspect
+            });
+            scene.add(selectBox);
+            // selectBox.matrix = newSelectBox.matrix;
+            // selectBox.geometry = newSelectBox.geometry;
+            // selectBox.material = newSelectBox.material;
+            // selectBox.remove(...selectBox.children);
+            // selectBox.add(...newSelectBox.children);
+
+
+
             // Only render if reasonable input values
             if ([props.channels, outputChannels(), kernelSize(), stride()].some(val => !val)) {
                 conv.visible = false;
@@ -412,18 +567,19 @@ export const Cube3D = (inProps: SvgCubeProps) => {
                 conv.visible = true;
                 // Update conv
                 let newconv = getConv2d(props.channels, outputChannels(), kernelSize(), stride(), padding(), dilation());
-                console.log("conv.children");
-                console.log(conv.children);
                 conv.remove(...conv.children);
                 conv.add(...newconv.children);
-                let pos = canvas2DToWorld3D(canvas.width / 2, 400 + 80 + 400/2, camera, canvas, scene);
+                let pos = canvas2DToWorld3D(canvas.width / 2, 400 + 80 + 400 / 2, camera, canvas, scene);
                 conv.position.set(pos.x, pos.y, pos.z);
                 // conv.scale.set(0.3, 0.3, 0.3)
             }
 
 
-            console.log("AAAAAAAAAA")
-            console.log(outputWidth(), outputHeight(), outputChannels())
+
+            let pos = canvas2DToWorld3D(canvas.width / 2, 400 + 80 + 400 / 2, camera, canvas, scene);
+            mesh.position.set(pos.x, pos.y, pos.z);
+
+
             // Only render if reasonable input values
             if ([outputWidth(), outputHeight(), outputChannels()].some(val => !val)) {
                 outputCube.visible = false;
@@ -434,12 +590,12 @@ export const Cube3D = (inProps: SvgCubeProps) => {
                 let newOutputCube = getCube(outputWidth(), outputHeight(), outputChannels(), ['#aaaacc', '#6677aa', '#ccddff', '#445577'], "#223344")
                 outputCube.remove(...outputCube.children);
                 outputCube.add(...newOutputCube.children);
-                let pos = canvas2DToWorld3D(canvas.width / 2, 400 + 80 + 400 + 80 + 400/2, camera, canvas, scene);
+                let pos = canvas2DToWorld3D(canvas.width / 2, 400 + 80 + 400 + 80 + 400 / 2, camera, canvas, scene);
                 outputCube.position.set(pos.x, pos.y, pos.z);
                 outputCube.scale.set(0.3, 0.3, 0.3)
             }
-            
-            
+
+
 
         });
     })
